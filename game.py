717620c8e34b73a9c2a1c049e668cb21e045a08d
Charlie You <charlie@charlieyou.me>
2016-11-11 19:46:34 -0500

    game wrapper

---
+++ game.py
@@ -0,0 +1,39 @@
+import requests
+
+
+class Game:
+    def __init__(self, devkey, username, practice=True):
+        if practice:
+            url = 'http://aicomp.io/api/games/practice'
+        else:
+            url = 'http://aicomp.io/api/games/search'
+
+        r = requests.post(url, data={'devkey': devkey, 'username': username})
+        json = r.json()
+
+        self.devkey = devkey
+        self.url = url + json['gameID']
+        self.playerID = json['playerID']
+
+    def _submit_move(self, move):
+        r = requests.post(self.url, data={'playerID': self.playerID,
+            'move': move, 'devkey': self.devkey})
+        return r.json()
+
+    def drop_bomb(self):
+        return self._submit_move('b')
+
+    def do_nothing(self):
+        return self._submit_move('')
+
+    def move(self, direction):
+        return self._submit_move('m' + direction[0])
+
+    def turn(self, direction):
+        return self._submit_move('t' + direction[0])
+
+    def shoot_portal(self, color):
+        return self._submit_move(color[0] + 'p')
+
+    def buy(self, power_up):
+        return self._submit_move('buy_' + power_up)
