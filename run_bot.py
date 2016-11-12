fd156142ca8ccdf88c3d404423de288cc45a7908
Max <mx@icj.me>
2016-11-11 21:55:37 -0500

    Added helpers for finding moves

--- run_bot.py
+++ run_bot.py
@@ -3,13 +3,37 @@ from game import Game
 devkey = '582632dd3c0cc0ed06f9d05a'
 username = 'mxms'
 
+def isTileInBoard(board, tile):
+	return false
+
+def isTileMoveable(board, loc):
+	return isTileInBoard(board, loc) and loc['x'] < 10 and loc['x'] > 0 and loc['y'] > 0 and loc['y'] < 10:
+
+def nearbyTiles(board, loc):
+	validTiles = []
+	leftTile = { 'x': loc['x'] - 1, 'y': loc['y'] }
+	rightTile = { 'x': loc['x'], 'y': loc['y'] + 1 }
+	topTile = { 'x': loc['x'], 'y': loc['y'] + 1 }
+	bottomTile = { 'x': loc['x'], 'y': loc['y'] - 1 }
+	if isTileMoveable(board, leftTile):
+		validTiles.append(leftTile)
+	if isTileMoveable(board, rightTile):
+		validTiles.append(rightTile)
+	if isTileMoveable(board, topTile):
+		validTiles.append(topTile)
+	if isTileMoveable(board, bottomTile):
+		validTiles.append(bottomTile)
+	return validTiles
 
 def findMove(game):
 	board = game.board
 	print game.playerLocation
 	print game.opponentLocation
 
-#	nearby = nearbyTiles(game.board, 
+	nearby = nearbyTiles(game.board, game.playerLocation)
+
+	for t in nearby:
+		print t
 
 if __name__ == '__main__':
 	game = Game(devkey, username, True)
